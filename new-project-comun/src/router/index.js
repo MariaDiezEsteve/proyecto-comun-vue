4001827e110086c9be2177e312a4dc68ee94c4bb
Juan-Little <juanbenitodevalle@gmail.com>
2023-03-07 20:08:56 +0100

    intento de colocar una navbar de bootstrap con muy poquito exito

--- new-project-comun/src/router/index.js
+++ new-project-comun/src/router/index.js
@@ -14,7 +14,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/navBar',
+    name: 'navBar',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/NavBarView.vue')
+  },
+  
 ]
 
 const router = createRouter({
