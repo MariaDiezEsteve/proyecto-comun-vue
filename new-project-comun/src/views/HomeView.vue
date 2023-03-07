5e1a6a1fda5c3d5a26aedc19f36c1d2137b358be
MariaDiezEsteve <diezesteve@outlook.es>
2023-03-07 13:38:28 +0100

    empezando card de la home para el proyecto en grupo comun de la exposición

---
+++ new-project-comun/src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="@/assets/imagenes/pinocho.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+</template>
+
+<script setup>
+
+const peliculas = [
+  {
+    name: "Pinocho",
+    src: "@/assets/imagenes/pinocho.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, sapiente."
+  },
+  {
+    name: "Wakanda Forever",
+    src: "@/assets/imagenes/wakanda.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, sapiente."
+  },
+  {
+    name: "Simpson",
+    src: "@/assets/imagenes/simpson.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, sapiente."
+  },
+]
+
+
+
+
+
+</script>
